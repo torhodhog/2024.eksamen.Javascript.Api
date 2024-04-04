1f042b125327ef71007ae2379cf68589a99a6770
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-04-04 11:27:17 +0200

    Added a options/select in HTML and image, name and type from API.

--- Task1/script.js
+++ Task1/script.js
@@ -1,7 +1,10 @@
+// Fetch the Pokémon
 fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
    .then(response => response.json())
    .then(data => {
       let container = document.getElementById('pokemon-container');
+      let types = new Set();
+
       data.results.forEach(pokemon => {
          fetch(pokemon.url)
             .then(response => response.json())
@@ -12,11 +15,15 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
                let h2 = document.createElement('h2');
                h2.textContent = pokeData.name;
                let p = document.createElement('p');
-               p.textContent = pokeData.types.map(typeInfo => typeInfo.type.name).join(', ');
+               let pokemonTypes = pokeData.types.map(typeInfo => typeInfo.type.name);
+               p.textContent = pokemonTypes.join(', ');
+               pokemonTypes.forEach(type => types.add(type));
                let buttonSave = document.createElement('button');
                buttonSave.textContent = 'Lagre';
                let buttonDelete = document.createElement('button');
                buttonDelete.textContent = 'Slett';
+
+               div.dataset.types = pokemonTypes.join(', ');
 
                div.appendChild(img);
                div.appendChild(h2);
@@ -27,5 +34,16 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
                container.appendChild(div);
             });
       });
+
+      setTimeout(() => {
+         let select = document.getElementById('type-filter');
+         types.forEach(type => {
+            let option = document.createElement('option');
+            option.value = type;
+            option.textContent = type;
+            select.appendChild(option);
+         });
+      }, 5000); 
    })
-   .catch(error => console.error('Det oppstod en feil:', error));
+   
+
